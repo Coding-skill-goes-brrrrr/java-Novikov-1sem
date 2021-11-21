e78fdb8ec4f38b98b2f0bede85fadc4db682b995
Default Username <boredom.lord@mail.ru>
2021-11-21 13:14:21 +0300

    Fixed

--- src/IntroTaskCat.java
+++ src/IntroTaskCat.java
@@ -8,11 +8,15 @@ public class IntroTaskCat {
         System.out.println(Cats(21));
         System.out.println(Cats(22));
         System.out.println(Cats(25));
+        System.out.println(Cats(111));
+        System.out.println(Cats(112));
+        System.out.println(Cats(113));
+        System.out.println(Cats(114));
     }
     public static String Cats (int x){
-        if ((x % 10 == 1)&&(x != 11))
+        if ((x % 10 == 1)&&(x % 100 != 11))
             return x + " кот";
-        else if ((x % 10 >= 2)&&(x % 10 <= 4)&&((x < 5)||(x > 20)))
+        else if ((x % 10 >= 2)&&(x % 10 <= 4)&&(x % 100 != 12)&&(x % 100 != 13)&&(x % 100 != 14))
             return x + " кота";
         else
             return x + " котов";
