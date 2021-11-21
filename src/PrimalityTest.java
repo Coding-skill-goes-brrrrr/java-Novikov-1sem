038f3843b5d8ccb9d67027ba3238a489d79bb2b3
Default Username <boredom.lord@mail.ru>
2021-11-21 13:16:28 +0300

    Fixed

--- src/PrimalityTest.java
+++ src/PrimalityTest.java
@@ -1,24 +1,24 @@
 public class PrimalityTest {
     public static void main(String[] args){
-        System.out.println(is_prime(1));
-        System.out.println(is_prime(2));
-        System.out.println(is_prime(3));
-        System.out.println(is_prime(4));
-        System.out.println(is_prime(20));
-        System.out.println(is_prime(21));
-        System.out.println(is_prime(23));
-        System.out.println(is_prime(37));
-        System.out.println(is_prime(42));
+        System.out.println(isPrime(1));
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(3));
+        System.out.println(isPrime(4));
+        System.out.println(isPrime(20));
+        System.out.println(isPrime(21));
+        System.out.println(isPrime(23));
+        System.out.println(isPrime(37));
+        System.out.println(isPrime(42));
     }
-    public static boolean is_prime (float x) {
+    public static boolean isPrime (float x) {
         if (x == 1) {
             System.out.println("Это единица -_-");
             return false;
         }
         else {
-            double x_help0 = Math.sqrt(x);
-            int x_help = (int) Math.round(x_help0);
-            for (int p = 2; p <= x_help; p = p + 1)
+            double xHelp0 = Math.sqrt(x);
+            int xHelp = (int) Math.round(xHelp0);
+            for (int p = 2; p <= xHelp; p = p + 1)
                 if (x % p == 0)
                     return false;
             return true;
