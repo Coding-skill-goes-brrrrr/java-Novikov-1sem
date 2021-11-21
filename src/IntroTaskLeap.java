fc54ffe9c89f76a18413930d856fd613b079bab8
Default Username <boredom.lord@mail.ru>
2021-11-21 13:26:24 +0300

    Fixed

--- src/IntroTaskLeap.java
+++ src/IntroTaskLeap.java
@@ -1,11 +1,11 @@
 public class IntroTaskLeap {
     public static void main(String[] args){
-        System.out.println(Is_Leap(1200));
-        System.out.println(Is_Leap(1449));
-        System.out.println(Is_Leap(2004));
-        System.out.println(Is_Leap(2005));
+        System.out.println(isLeap(1200));
+        System.out.println(isLeap(1449));
+        System.out.println(isLeap(2004));
+        System.out.println(isLeap(2005));
     }
-    public static boolean Is_Leap (int x){
+    public static boolean isLeap (int x){
         if (x % 100 == 0)
             return (x % 400 == 0);
         else
