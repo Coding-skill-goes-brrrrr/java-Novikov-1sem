2536a00dc5bbbbcab39ea50d6fec507cf120d8d0
Default Username <boredom.lord@mail.ru>
2021-12-07 20:19:20 +0300

    Tests with number matrix and trigonometric matrix

--- src/Tests.java
+++ src/Tests.java
@@ -1,13 +1,17 @@
 public class Tests {
     public static void main(String[] args) {
         Matrix22 matrica = new Matrix22(1, 2, 3, 4);
+        matrica.print();
+        System.out.println("----------------------------");
+        Matrix22 matrica_kak_massiv = new Matrix22(new double [][] {{100, 2},{3, 400}});
+        matrica_kak_massiv.print();
+        System.out.println(matrica.getMatrix22());
+        System.out.println(matrica_kak_massiv.getMatrix22());
+        System.out.println("----------------------------");
         System.out.println(matrica.trace());
         System.out.println(matrica.determinant());
-
         System.out.println(matrica); //я честно не знаю, как это работает: IDEA говорит, что "toString() - unnecessary"
         System.out.println(matrica.toString()); //и почему-то эти два варианта равнозначны независимо от знаков между элементами матрицы
-        System.out.println("----------------------------");
-        matrica.print();
         System.out.println("----------------------------");
         Matrix22 dlya_arifmetiki = new Matrix22(100000, 2, 3, 400);
         matrica.add(dlya_arifmetiki).print();
