f7c1f08848cc4d644fe37d0a9ef862678b05146e
Default Username <boredom.lord@mail.ru>
2021-12-07 18:59:15 +0300

    Task about inheritance

---
+++ src/Rotation.java
@@ -0,0 +1,10 @@
+public class Rotation extends Matrix22 {
+    private double ugol;
+    public Rotation(double a) {
+        super(Math.sin(Math.toRadians(a)), Math.cos(Math.toRadians(a)), -Math.cos(Math.toRadians(a)), Math.sin(Math.toRadians(a)));
+        this.ugol = a;
+    }
+    public double getAngle(){
+        return ugol;
+    }
+}
